f7abcd6f621f9be7ba4272bcbbe18cd39973d717
Oskar <oskar@newfantastic.com>
2020-02-13 03:28:46 +0100

    Fetch target data to the app

---
+++ src/store/mutations.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+const mutations = {
+  getWeatherByCoords(state, coords) {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${coords.latitude}&lon=${coords.longitude}&appid=b48493f02664a91b20a3fd845b9f9ff3`
+      )
+      .then(res => {
+        if (state.weather) {
+          state.searchHistory = [...state.searchHistory, state.weather];
+        }
+        state.weather = res.data;
+      })
+      .catch(err => {
+        console.log("An error occured: ", err);
+      });
+  },
+  getWeatherByCity(state, city) {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b48493f02664a91b20a3fd845b9f9ff3`
+      )
+      .then(res => {
+        state.searchHistory = [...state.searchHistory, state.weather];
+        state.weather = res.data;
+      })
+      .catch(err => {
+        console.log("An error occured: ", err);
+      });
+  },
+  setLocation(state, coords) {
+    console.log(coords);
+    state.location = {
+      latitude: coords.latitude,
+      longitude: coords.longitude
+    };
+  },
+  changeMetricSystem(state) {
+    state.metricSystem === "Imperial"
+      ? (state.metricSystem = "Metric")
+      : (state.metricSystem = "Imperial");
+  }
+};
+
+export default mutations;
